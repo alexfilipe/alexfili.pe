f7774c3c8c42c77542d62987643daa89f17576de
Álex Filipe <hi@alexfili.pe>
2026-06-30 17:01:12 -0700

    Add afcbs domain redirect worker

---
+++ scripts/afcbs-redirect-worker.js
@@ -0,0 +1,18 @@
+const TARGET_ORIGIN = "https://alexfili.pe";
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+function handleRequest(request) {
+  const url = new URL(request.url);
+  const location = `${TARGET_ORIGIN}${url.pathname}${url.search}`;
+
+  return new Response(null, {
+    status: 308,
+    headers: {
+      location,
+      "cache-control": "public, max-age=3600",
+    },
+  });
+}
